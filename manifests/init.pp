eb86ac860855063feeba4643e48d2952ec4121ed
Gabriel R F <github@gabrf.com>
2015-05-26 15:59:47 -0300

    Bug fix

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,72 +1,72 @@
 #init.pp
 class postgresreplication (
-  $slave                = false,
+#  $slave                = false,
   $user                 = 'rep',
   $master_IP_address,
   $slave_IP_address,
   $port                 = 5432,
   $password,
-  $trigger_file         = "/etc/postgresql/this.is.master",
+  $trigger_file         = undef,
 )
 {
   validate_bool($slave)
   #validate_integer($port)
   validate_bool(is_ip_address($master_IP_address))
   validate_bool(is_ip_address($slave_IP_address))
-  ##postgresql.conf
-  postgresql::server::config_entry { 'wal_level':
-    value => 'hot_standby',
-  }
-  postgresql::server::config_entry { 'archive_mode':
-    value => 'on',
-  }
-  postgresql::server::config_entry { 'archive_command':
-    value => 'cd .',
-  }
-  postgresql::server::config_entry { 'max_wal_senders':
-    value => '1',
-  }
-  postgresql::server::config_entry { 'hot_standby':
-    value => 'on',
-  }
-
-  ## SLAVE CONFIGURATION
-  ##if $slave == true {
   if $::ipaddress == $slave_IP_address {
     class { 'postgresql::server':
-      ipv4acls       => ["host replication $user $master_IP_address/32 md5"],                    ##pg_hba.conf
-      listen_addresses => "localhost,$slave_IP_address",                                         ##postgresql.conf
+      ipv4acls       => ["host replication $user $master_IP_address/32 md5"],
+      listen_addresses => "localhost,$slave_IP_address",
       manage_recovery_conf => true,
-    }
-    file { '/etc/postgresql/this.is.master':
-      ensure => 'absent',
     }
     postgresql::server::recovery { 'postgresrecovery':
       standby_mode => 'on',
       primary_conninfo => "host=$master_IP_address port=$port user=$user password=$password",
       trigger_file => "$trigger_file",
     }
+    postgresql::server::config_entry { 'wal_level':
+      value => 'hot_standby',
+    }
+    postgresql::server::config_entry { 'archive_mode':
+      value => 'on',
+    }
+    postgresql::server::config_entry { 'archive_command':
+      value => 'cd .',
+    }
+    postgresql::server::config_entry { 'max_wal_senders':
+      value => '1',
+    }
+    postgresql::server::config_entry { 'hot_standby':
+      value => 'on',
+    }
   }
-
-  ## MASTER CONFIGURATION
   else {
     class { 'postgresql::server':
-      ipv4acls        => ["host replication $user $slave_IP_address/32 md5"],                    ##pg_hba.conf
-      listen_addresses  => "localhost,$master_IP_address",                                       ##postgresql.conf
-      manage_recovery_conf => true,
+      ipv4acls        => ["host replication $user $slave_IP_address/32 md5"],
+      listen_addresses  => "localhost,$master_IP_address",
     }
-    file { '/etc/postgresql/this.is.master':
-      ensure => 'present',
+    file { '/var/lib/postgresql/9.3/main/recovery.conf':
+      ensure => 'absent',
     }
     postgresql::server::role { "$user":
       password_hash      => postgresql_password("$user", "$password"),
         replication      => true,
         connection_limit => 1,
     }
-    postgresql::server::recovery { 'postgresrecovery':
-      standby_mode => 'on',
-      primary_conninfo => "host=$slave_IP_address port=$port user=$user password=$password",
-      trigger_file => "$trigger_file",
+    postgresql::server::config_entry { 'wal_level':
+      value => 'hot_standby',
+    }
+    postgresql::server::config_entry { 'archive_mode':
+      value => 'on',
+    }
+    postgresql::server::config_entry { 'archive_command':
+      value => 'cd .',
+    }
+    postgresql::server::config_entry { 'max_wal_senders':
+      value => '1',
+    }
+    postgresql::server::config_entry { 'hot_standby':
+      value => 'on',
     }
   }
 }
